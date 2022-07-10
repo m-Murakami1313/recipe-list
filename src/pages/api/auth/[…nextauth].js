cea12f2a8247dd8cf48d732830316c463d64d251
m-Murakami1313 <mituvv@gmail.com>
2022-07-10 11:26:05 +0900

    next-authの追加

---
+++ src/pages/api/auth/[…nextauth].js
@@ -0,0 +1,23 @@
+import { PrismaAdapter } from "@next-auth/prisma-adapter"
+import { PrismaClient } from "@prisma/client"
+import NextAuth from "next-auth"
+import GitHubProvider from "next-auth/providers/github";
+
+const prisma = new PrismaClient()
+
+export default NextAuth({
+  adapter: PrismaAdapter(prisma),
+  providers: [
+    GitHubProvider({
+      clientId: process.env.GITHUB_ID,
+      clientSecret: process.env.GITHUB_SECRET
+    })
+  ],
+  callbacks: {
+    async session({ session, user }) {
+      session.user.id = user.id;
+      return Promise.resolve(session);
+    }
+  }
+ 
+})
