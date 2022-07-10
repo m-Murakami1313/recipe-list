c96331b99a7768909cf8cf7150e1138d58d4116f
m-Murakami1313 <mituvv@gmail.com>
2022-07-10 16:40:50 +0900

    ホーム画面の作成

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,14 +1,14 @@
 import type { NextPage } from 'next'
-import Head from 'next/head'
-import Image from 'next/image'
 import { Layout } from '@/components/Layout'
 
 const Home: NextPage = () => {
   return (
     <div>
-      <Layout title="Home">
-        
-
+      <Layout title='Home'>
+        <div className='mt-20 text-center'>
+          <h1 className='py-16 text-3xl lg:text-4xl'>夜ごはんのメニューを簡単に決めませんか？</h1>
+          <h2 className='py-16 text-2xl lg:text-3xl'>使い方</h2>
+        </div>
       </Layout>
     </div>
   )
