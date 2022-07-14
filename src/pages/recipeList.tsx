96e884789d2943b98cd46eb146640f7dd9815553
m-Murakami1313 <mituvv@gmail.com>
2022-07-14 21:35:04 +0900

    fix 使用していた関数をカスタムフック化し、再利用できるようにした。

--- src/pages/recipeList.tsx
+++ src/pages/recipeList.tsx
@@ -4,52 +4,26 @@ import { useEffect, useState } from 'react'
 import { Layout } from '@/components/Layout'
 
 import { Table } from '@/components/UI/atom/Table'
-import { useDeleteRecipe } from '@/hooks/useDeleteRecipe'
+import { SearchForm } from '@/components/UI/organisms/SearchForm'
+import { useRecipeList } from '@/hooks/useRecipeList'
 import { initialDataList, options } from '@/libs/tableData'
 
 const RecipeList: NextPage = () => {
-  const [recipes, setRecipes] = useState<any>([])
-  const [pickDayId, setPickDayId] = useState<number>(0)
-  const [recipeFlag, setRecipeFlag] = useState(false)
-
-  const { tableData, setTableData, deleteRecipe } = useDeleteRecipe()
+  const {
+    getRecipe,
+    getTarget,
+    onSubmitList,
+    deleteRecipe,
+    tableData,
+    setTableData,
+    recipes,
+    recipeFlag,
+    setPickDayId,
+  } = useRecipeList()
 
   useEffect(() => {
     setTableData(initialDataList)
   }, [])
-
-  const getTarget = (e: any) => {
-    const recipeData = [...recipes]
-    const targetData = recipeData[e.target.id]
-    setRecipes([targetData])
-    setRecipeFlag(true)
-  }
-
-  const onSubmitList = (e: any) => {
-    setRecipeFlag(false)
-    const oldData = [...tableData]
-    const newData = { ...oldData[pickDayId - 1], recipe: recipes[0].name }
-    const newDataSet = oldData.map((data) => (data.tableId === Number(pickDayId) ? newData : data))
-    setTableData(newDataSet)
-    setRecipes([])
-    setPickDayId(0)
-  }
-
-  const getRecipe = async (e: any) => {
-    e.preventDefault()
-    setRecipeFlag(false)
-    const response = await fetch('api/recipe', {
-      method: 'GET',
-    })
-    const json = await response.json()
-    if (response.ok) {
-      console.log(response.status)
-      console.log(json)
-      setRecipes(json)
-    } else {
-      console.log(response.status)
-    }
-  }
 
   return (
     <div>
@@ -60,45 +34,7 @@ const RecipeList: NextPage = () => {
         <div className='flex items-center justify-center'>
           <div>
             <div className='mt-10'>
-              <form className='flex items-center justify-center'>
-                <label
-                  htmlFor='default-search'
-                  className='flex h-12 w-24 items-center justify-center rounded bg-gray-100 text-sm md:mr-5 md:h-14 md:w-24'
-                >
-                  レシピ検索
-                </label>
-                <div className='relative w-96'>
-                  <div className='pointer-events-none absolute inset-y-0 left-0 flex  items-center pl-3'>
-                    <svg
-                      className='h-5 w-5 text-gray-500'
-                      fill='none'
-                      stroke='currentColor'
-                      viewBox='0 0 24 24'
-                      xmlns='http://www.w3.org/2000/svg'
-                    >
-                      <path
-                        strokeLinecap='round'
-                        strokeLinejoin='round'
-                        strokeWidth='2'
-                        d='M21 21l-6-6m2-5a7 7 0 11-14 0 7 7 0 0114 0z'
-                      ></path>
-                    </svg>
-                  </div>
-                  <input
-                    type='search'
-                    id='default-search'
-                    className='block w-full rounded-lg border border-gray-300 bg-gray-50 p-4 pl-10 text-sm text-gray-900'
-                    placeholder=''
-                    required
-                  />
-                  <button
-                    className='absolute right-2.5 bottom-2.5 rounded-lg bg-blue-700 px-4 py-2 text-sm font-medium text-white'
-                    onClick={getRecipe}
-                  >
-                    Search
-                  </button>
-                </div>
-              </form>
+              <SearchForm getData={getRecipe} />
             </div>
           </div>
         </div>
