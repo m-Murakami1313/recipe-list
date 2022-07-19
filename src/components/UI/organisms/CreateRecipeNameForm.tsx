125ddc62debe6698d0fc9c91c47b00801104c181
m-Murakami1313 <mituvv@gmail.com>
2022-07-19 18:32:14 +0900

    fix inputフォームを再利用できるようにコンポーネント化した。

--- src/components/UI/organisms/CreateRecipeNameForm.tsx
+++ src/components/UI/organisms/CreateRecipeNameForm.tsx
@@ -1,19 +1,13 @@
+import { Input } from '../atom/Input'
+
 export const CreateRecipeNameForm = ({ setRecipeName, recipeName }: any) => {
+  const placeholder = 'レシピ名を記入'
   return (
     <div>
       <label htmlFor='name' className='text-2xl'>
         レシピ名
       </label>
-      <input
-        id='name'
-        type='text'
-        className='mt-5 block w-96 rounded-lg border border-gray-300 bg-gray-50 p-2.5 text-lg text-gray-900 '
-        placeholder='レシピ名を記入'
-        value={recipeName}
-        onChange={(e: any) => {
-          setRecipeName(e.target.value)
-        }}
-      />
+      <Input textValue={recipeName} handleOnChange={setRecipeName} placeholder={placeholder} />
     </div>
   )
 }
