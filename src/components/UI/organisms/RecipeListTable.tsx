f0ad16ad452212d17539ac7603183cd4e2192fe7
m-Murakami1313 <mituvv@gmail.com>
2022-07-23 18:27:11 +0900

    fix レシピリストを表示するページを作成 テーブルで表示を行った

--- src/components/UI/organisms/RecipeListTable.tsx
+++ src/components/UI/organisms/RecipeListTable.tsx
@@ -1,16 +1,19 @@
+import { useRouter } from 'next/router'
 import React from 'react'
 import { TABLE_DEFINE_List } from '../../../libs/tableData'
 
 export const RecipeListTable = ({ tableData }: any) => {
+  const router = useRouter()
+
   return (
-    <div className='relative w-full overflow-x-auto shadow-md sm:rounded-lg lg:w-4/5'>
+    <div className='relative w-full overflow-x-auto shadow-md sm:rounded-lg lg:w-2/3'>
       <table className='w-full text-gray-500'>
         <thead className='h-20 bg-gray-50 text-sm uppercase text-gray-700 md:text-lg'>
           <tr>
             {TABLE_DEFINE_List.map((def) => {
               if (def.key === 'dayOfWeek') {
                 return (
-                  <th className='w-1/6 px-6 py-3 text-center' key={def.key}>
+                  <th className='w-1/3 px-6 py-3 text-center' key={def.key}>
                     {def.label}
                   </th>
                 )
@@ -25,10 +28,15 @@ export const RecipeListTable = ({ tableData }: any) => {
         </thead>
         <tbody className='text-sm md:text-lg'>
           {tableData &&
-            tableData.map((day: any, index: any) => (
-              <tr key={day.day} className='h-10 border-b text-center'>
-                <th>{day.day}</th>
-                <td>{day.recipe}</td>
+            tableData.map((data: any) => (
+              <tr key={data.recipeId} className='h-10 border-b text-center'>
+                <th>{data.dayOfWeek}</th>
+                <td
+                  className='hover:cursor-pointer hover:text-blue-800 hover:underline'
+                  onClick={() => router.push(`../../recipes/${data.recipeId}`)}
+                >
+                  {data.recipe.recipeName}
+                </td>
               </tr>
             ))}
         </tbody>
