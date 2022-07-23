529b2a568b548d60792bfaddb940183ed6bfe5d6
m-Murakami1313 <mituvv@gmail.com>
2022-07-23 19:59:49 +0900

    add レシピの検索ページを作成

---
+++ src/components/UI/templates/SearchRecipes.tsx
@@ -0,0 +1,27 @@
+import Link from 'next/link'
+import React from 'react'
+import { SearchForm } from '@/components/UI/organisms/SearchForm'
+import { useRecipeList } from '@/hooks/useRecipeList'
+
+export const SearchRecipes = () => {
+  const { recipes, getRecipe, searchValue, setSearchValue } = useRecipeList()
+  return (
+    <div className='mt-10'>
+      <SearchForm getData={getRecipe} searchValue={searchValue} setSearchValue={setSearchValue} />
+      <div className='mt-5'>
+        <p>検索結果</p>
+        <ul>
+          {recipes.map((recipe: any) => (
+            <div key={recipe.id} className='flex items-center py-4'>
+              <Link href={`../../recipes/${recipe.id}`}>
+                <a>
+                  <li className='hover:text-blue-800 hover:underline'>{recipe.recipeName}</li>
+                </a>
+              </Link>
+            </div>
+          ))}
+        </ul>
+      </div>
+    </div>
+  )
+}
