07473e063a42105d62687b377ebc66544fb73cdf
m-Murakami1313 <mituvv@gmail.com>
2022-07-27 20:25:04 +0900

    fix list_recipeテーブルに関してのレシピがnullの場合でも、通る様にした

---
+++ prisma/migrations/20220727094912_recipe_null/migration.sql
@@ -0,0 +1,8 @@
+-- DropForeignKey
+ALTER TABLE `list_recipe` DROP FOREIGN KEY `List_Recipe_recipeId_fkey`;
+
+-- AlterTable
+ALTER TABLE `list_recipe` MODIFY `recipeId` VARCHAR(191) NULL;
+
+-- AddForeignKey
+ALTER TABLE `List_Recipe` ADD CONSTRAINT `List_Recipe_recipeId_fkey` FOREIGN KEY (`recipeId`) REFERENCES `Recipe`(`id`) ON DELETE SET NULL ON UPDATE CASCADE;
