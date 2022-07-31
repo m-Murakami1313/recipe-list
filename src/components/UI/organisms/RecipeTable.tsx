1680bbe62129ec66c3a812f639561d0194a6c837
m-Murakami1313 <mituvv@gmail.com>
2022-07-31 19:09:27 +0900

    fix　テーブルのレイアウトを修正した。

--- src/components/UI/organisms/RecipeTable.tsx
+++ src/components/UI/organisms/RecipeTable.tsx
@@ -3,10 +3,10 @@ import React from 'react'
 export const RecipeTable = ({ tableData, tableHead }: any) => {
   return (
     <div className='relative w-full overflow-x-auto shadow-md sm:rounded-lg lg:w-4/5'>
-      <table className='w-full text-gray-500'>
-        <thead className='h-20 bg-gray-50 text-sm uppercase text-gray-700 md:text-lg'>
+      <table className='w-full table-fixed text-gray-500'>
+        <thead className='h-20 bg-gray-50 text-sm uppercase text-gray-700 md:text-lg '>
           <tr>
-            {tableHead.map((data: any,index:number) => (
+            {tableHead.map((data: any, index: number) => (
               <th key={index}>{data}</th>
             ))}
           </tr>
