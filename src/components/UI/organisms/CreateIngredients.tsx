0f5dc5a4992c4ced5f9017fb0a14dd4971a4b54e
m-Murakami1313 <mituvv@gmail.com>
2022-08-12 18:23:36 +0900

    fix CreateIngredientsのvalidationの修正　および修正に伴うレイアウトの変更

--- src/components/UI/organisms/CreateIngredients.tsx
+++ src/components/UI/organisms/CreateIngredients.tsx
@@ -29,27 +29,29 @@ export const CreateIngredients = ({
       </div>
       <div className='mt-5'>
         {createIngredientsDataSet.map((data: createIngredientsTypes, index: number) => (
-          <div key={index} className='md:flex'>
+          <div key={index} className=''>
             <input
               id={index.toString()}
               className='block w-80 rounded-lg border border-gray-300 bg-gray-50 p-2.5 text-base text-gray-900  md:text-lg '
               placeholder='調味料・食材'
               onChange={handleChangeIngredientsName}
               value={data.ingredientsName}
-              maxLength={15}
+              maxLength={20}
             />
+              {data.ingredientsName.length >= 20 && <p className='text-red-500'>２０文字以内で入力してください </p>}
             <input
               id={index.toString()}
               className='block w-80 rounded-lg border border-gray-300 bg-gray-50 p-2.5 text-base text-gray-900  md:text-lg'
               placeholder='使用量'
               onChange={handleChangeIngredientsWeight}
               value={data.weight}
-              maxLength={15}
-            />
+              maxLength={20}
+              />
+              {data.weight.length >= 20 && <p className='text-red-500'>２０文字以内で入力してください </p>}
             <button
-              className='btn btn-ghost bg-yellow-400 p-2 md:ml-5'
+              className='btn btn-ghost bg-yellow-400 p-2'
               onClick={deleteIngredientsData}
-            >
+              >
               欄削除
             </button>
           </div>
